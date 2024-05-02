8e015958657f68a28ee034e954f416aea642a60f
Rui Silva <ruidsoaressilva@icloud.com>
2024-05-02 02:15:38 +0100

    BSHARD-3 Add Create Shard modal and test input

---
+++ src/views/NewShardModal.ts
@@ -0,0 +1,42 @@
+import {App, Modal, Setting} from "obsidian";
+
+export class NewShardModal extends Modal {
+	result: string;
+	private onSubmit: (result: string) => void;
+
+	constructor(app: App, onSubmit: (result: string) => void) {
+		super(app);
+		this.onSubmit = onSubmit;
+	}
+
+
+	onOpen() {
+		const {contentEl} = this;
+
+		contentEl.createEl("h3", {text: "New Shard"});
+
+		new Setting(contentEl)
+			.setName("New Shard Name")
+			.addText((text) =>
+				text.onChange((value) => {
+					this.result = value;
+				}));
+
+		new Setting(contentEl)
+			.addButton((btn) =>
+				btn
+					.setButtonText("Create")
+					.setCta()
+					.onClick(() => {
+							this.close();
+							this.onSubmit(this.result);
+						}
+					)
+			);
+	}
+
+	onClose() {
+		const {containerEl} = this;
+		containerEl.empty();
+	}
+}
