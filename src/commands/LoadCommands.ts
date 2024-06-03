87f1fecdd111105ec55ec912249de0bc4188fbbe
Rui Silva <ruidsilva@outlook.com>
2024-06-03 17:02:23 +0100

    BSHARD-4 Apply template when creating Shard

--- src/commands/LoadCommands.ts
+++ src/commands/LoadCommands.ts
@@ -2,5 +2,5 @@ import {NewShardCommand} from "./NewShardCommand";
 import BrainShardPlugin from "../main";
 
 export function loadCommands(plugin: BrainShardPlugin) {
-	plugin.addCommand(new NewShardCommand(plugin.app, plugin.settings.shardStorage));
+	plugin.addCommand(new NewShardCommand(plugin.app, plugin.settings.shardStorage, plugin.settings.shardTemplate));
 }
--- src/commands/NewShardCommand.ts
+++ src/commands/NewShardCommand.ts
@@ -1,4 +1,4 @@
-import {App, Command} from "obsidian";
+import {App, Command, TFile} from "obsidian";
 import {NewShardModal} from "../views/NewShardModal";
 
 export class NewShardCommand implements Command {
@@ -6,17 +6,26 @@ export class NewShardCommand implements Command {
 	name = "New Shard";
 	app: App;
 	shardPath: string;
+	shardTemplate: string;
 
-	constructor(app: App, shardPath: string) {
+	constructor(app: App, shardPath: string, shardTemplate: string) {
 		this.app = app;
 		this.shardPath = shardPath;
+		this.shardTemplate = shardTemplate;
 	}
 
 	callback(): void {
 	console.log("New Shard Command");
-		new NewShardModal(this.app, result => {
-			console.log("New Shard Command", result);
-			this.app.vault.create(`${this.shardPath}/${result}.md`, "").then(file => {
+
+		new NewShardModal(this.app, async result => {
+			console.log("Shard name", result);
+
+			//get template content
+			//todo: In the interest of robustness we shouldn't assume the template path exists or is a valid Markup file
+			const templateContent: string = await this.app.vault.cachedRead((this.app.vault.getAbstractFileByPath(this.shardTemplate) as TFile));
+
+			//todo: It could be interesting to at least perform the basic template parsing that plain Obsidian temaplating engine does.
+			this.app.vault.create(`${this.shardPath}/${result}.md`, templateContent).then(file => {
 				console.log("Shard Created");
 				//Todo: Maybe add a setting to choose whether the user wants to open the file or not.
 				this.app.workspace.getLeaf().openFile(file)
